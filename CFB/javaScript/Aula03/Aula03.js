ba8623358c43c4d7facd17c8addf4efcc5e7aa79
soueuwilliam <39564940+soueuwilliam@users.noreply.github.com>
2024-01-02 19:45:46 +0000

    Adicionando aulas 1 , 2 ,3 do curso do CFB

---
+++ CFB/javaScript/Aula03/Aula03.js
@@ -0,0 +1,20 @@
+"use strict"
+var nomeComVar = 'William com var';
+let nomeComLet = 'William com let';
+const nomeComConst = 'William com Const';
+
+
+function teste(){
+  let nome = 'william'
+  if(true){
+    console.log('Dentro do IF do teste: ' + nome);
+  }
+  console.log('dentro do teste: ' + nome)
+  
+}
+console.log(nomeComVar);
+console.log(nomeComLet)
+console.log(nomeComConst)
+
+
+
